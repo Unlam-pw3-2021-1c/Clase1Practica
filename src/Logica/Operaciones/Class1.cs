3a982c48f445ce9c41ffb3c7a303466175334444
Pablo Sanchez <psanchez@plus54.com>
2021-04-07 20:44:53 -0300

    agregando proy consola, proy WPF WinApp y Logica.csproj Calculadora sumar 2 numeros

---
+++ src/Logica/Operaciones/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.Operaciones
+{
+    public class Class1
+    {
+    }
+}
