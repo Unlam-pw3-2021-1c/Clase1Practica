2fa3fe94506fa520ce538f4571019817fcea0a8e
Pablo Sanchez <psanchez@plus54.com>
2021-04-14 19:45:48 -0300

    Implementacion simple de ordenar 5 nombres ingresados por pantalla

---
+++ src/Logica/EjercicioOrdenarNombres.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class EjercicioOrdenarNombres
+    {
+        static readonly int cantNombres = 5;
+
+        public static void Ejecutar()
+        {
+            List<string> nombres = new List<string>();
+
+            for (int i = 0; i < cantNombres; i++)
+            {
+                Console.WriteLine($"Ingrese Nombre {i+1}: ");
+                string nombre = Console.ReadLine();
+                nombres.Add(nombre);
+            }
+            nombres.Sort();
+
+            Console.WriteLine();
+            Console.WriteLine("La lista de nombres ordenada es:");
+            for (int i = 0; i < nombres.Count; i++)
+            {
+                Console.WriteLine(nombres[i]);
+            }
+        }
+    }
+}
--- src/PrimeraClase20211C/Program.cs
+++ src/PrimeraClase20211C/Program.cs
@@ -8,12 +8,20 @@ namespace PrimeraClase20211C
     {
         static void Main(string[] args)
         {
+            //EjercicioSumarNros();
+
+            EjercicioOrdenarNombres.Ejecutar();
+
+        }
+
+        private static void EjercicioSumarNros()
+        {
             Console.WriteLine("Ingrese un nro:");
             string num1 = Console.ReadLine();//1
 
             Console.WriteLine("Ingrese otro nro:");
             string num2 = Console.ReadLine();//2
-            
+
             int resultado = Calculadora.Sumar(num1, num2);
             Console.WriteLine("La suma de ambos nros es: " + resultado);//12
         }
